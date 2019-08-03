ead8c1100782f84ceb7858ebba84edb260bdcc5f
hiaximize <dbarkerit@gmail.com>
2019-08-03 14:31:43 -0400

    pushing app that is currently writing to cloud database to herokue to trouble shoot failed pushes with project while on heroku

--- server.js
+++ server.js
@@ -1,101 +1,24 @@
-//___________________
-//Dependencies
-//___________________
 const express = require('express');
-const cmd = require('node-cmd')
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const user = require('./models/users.js')
-const app = express ();
-const db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
-//___________________      
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
+const app = express();
 const MONGODB_URI = process.env.MONGODB_URI;
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
-// Fix Depreciation Warnings from Mongoose*
-// May or may not need these depending on your Mongoose version
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+// this must be below the middleware. Its going to go into the fruits.js files before it has a chance to get to the middleware
+const fruitsController = require('./controllers/fruits.js');
 
-// Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
-//___________________
-//Middleware
-//___________________
-
-//use public folder for static assets
-app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+// once we require the fruitsController we have to 'use' it before it's acceptable. By having '/fruits/' prarameter in here, we can remove /fruits from all the routers routes in fruits.js. It tells the express app that everything will automatically include /fruits. 
+app.use('/fruits/', fruitsController);
 
 
-//___________________
-// Routes
-//___________________
-
-
-
-
-//localhost:3000
-app.get('/', (req, resp) => {
-    
-    resp.render('index.ejs');
-    
+app.listen(3000, () => {
+    console.log('listening...');
 });
 
-
- 
-app.post('/call', (req, resp)=>{
-
-    
-    user.create({auth_token: req.body.authToken,
-        sid: req.body.sid,
-        phoneNumber: req.body.phoneNumber,
-        message: req.body.message}, (error, newData)=>{
-            if (error){
-                console.log(error);
-                } 
-            else {
-                console.log(newData);
-                
-                resp.redirect('/call');
-            }
-    });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
 });
-
-app.get('/call', (req,resp)=>{
-    const accountSid = req.body.sid;
-    const authToken = req.body.auth_token;
-    const client = require('twilio')(accountSid, authToken);
-
-    client.messages(req.body.message)
-      .fetch()
-      .then(message => console.log(message.body));
-    resp.redirect('/');
-})
-
-
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
-
