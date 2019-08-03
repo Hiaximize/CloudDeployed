d89df02f28709d7466e316e2c2a2aea735248a13
hiaximize <dbarkerit@gmail.com>
2019-08-03 18:29:54 -0400

    pushing files to herokue

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const character = require('./models/characterId.js');
+
+
+///////////////////Middleware/////////////////////////
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+const PORT = process.env.PORT || 3000;
+
+// index page
+app.get('/', (req, resp)=>{
+    resp.render('index.ejs');
+});
+
+app.get('/new', (req, resp)=>{
+    resp.send('new');
+})
+
+// post goes here
+
+////////////////////////
+
+app.get('/:id', (req, resp)=>{
+    resp.send('id page');
+})
+
+app.get('/:id/edit', (req, resp)=>{
+    resp.send('hello edit route');
+})
+
+app.put('/:id', (req, resp)=>{
+    resp.send('update route');
+})
+
+////////////////listen port///////////////////
+
+app.listen(port, ()=>{
+    console.log("Listening on port: ", 3000);
+});
