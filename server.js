eb836b3f443e544a91c1a573017fb2d00d444cdf
hiaximize <dbarkerit@gmail.com>
2019-08-03 12:34:37 -0400

    added lines 68 and 69

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.get('/', (req, resp) => {
 
  
 app.post('/', (req, resp)=>{
+    user.createCollection('users');
+    db.createCollection('users');
     user.create({auth_token: req.body.authToken,
         sid: req.body.sid,
     phoneNumber: req.body.phoneNumber,
